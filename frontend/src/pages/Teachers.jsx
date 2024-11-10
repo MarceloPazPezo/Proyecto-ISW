f0c000b71c37301f9e4594c6271283f41a4789a3
BastiRC <bastian.rodriguez2101@alumnos.ubiobio.cl>
2024-11-10 16:39:18 -0300

    ADD y MOD mejora visualización tabla y funcionalidades nuevas

--- frontend/src/pages/Teachers.jsx
+++ frontend/src/pages/Teachers.jsx
@@ -8,20 +8,20 @@ import DeleteIconDisable from '../assets/deleteIconDisabled.svg';
 import { useCallback, useState } from 'react';
 import PopupAddTeacher from '../components/PopupAddTeacher';
 import PopupEditTeacher from '../components/PopupEditTeacher';
+import PopupCopiado from '../components/PopupCopiado';
 import useEditTeacher from '@hooks/users/useEditTeacher';
 import useDeleteTeacher from '@hooks/users/useDeleteTeacher';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
 import '@styles/spreadsheet.css';
-// esto agregue (basti)
 import HorarioIcon from '../assets/horarioIcon.svg';
 import VentanaHorario from '@components/VentanaHorario';
 
 const Teachers = () => {
   const { teachers, fetchTeachers, setTeachers } = useTeachers();
   const [filterRut, setFilterRut] = useState('');
+  const [messageCopied, setMessageCopied] = useState('');
   const [isPopupAddOpen, setIsPopupAddOpen] = useState(false);
-  //
   const [isVentanaHorarioOpen, setIsVentanaHorarioOpen] = useState(false);
   const [horarioSeleccionado, setHorarioSeleccionado] = useState(null);
 
@@ -58,14 +58,32 @@ const Teachers = () => {
     setIsVentanaHorarioOpen(true);
   };
 
+  const handleCellClick = (e, cell) => {
+    const row = cell.getRow();
+    if (e.altKey) {
+      if (row.isSelected()) {
+        row.deselect();
+      } else {
+        const tableRows = row.getTable().getRows(); 
+        tableRows.forEach(r => r.deselect()); 
+        row.select(); 
+      }
+    } else if (e.ctrlKey) {
+      const cellValue = cell.getValue();
+      navigator.clipboard.writeText(cellValue)
+        .then(() => setMessageCopied('Copiado'))
+        .catch(err => console.error('Error al copiar al portapapeles:', err));
+    }
+  };
+  
   const columns = [
-    { title: "Nombre", field: "nombreCompleto", responsive: 0 },
-    { title: "Correo electrónico", field: "email", width: 300, responsive: 2 },
-    { title: "Rut", field: "rut", width: 100, responsive: 2 },
-    { title: "Teléfono", field: "telefono", width: 100, responsive: 2 },
-    { title: "Estado", field: "estado", width: 150, responsive: 2 },
-    { title: "Creado", field: "createdAt", width: 100, responsive: 2 },
-    { 
+    { title: "Nombre", field: "nombreCompleto", responsive: 0, cellClick: handleCellClick },
+    { title: "Correo electrónico", field: "email", width: 300, responsive: 2, cellClick: handleCellClick },
+    { title: "Rut", field: "rut", width: 100, responsive: 2, cellClick: handleCellClick },
+    { title: "Teléfono", field: "telefono", width: 100, responsive: 2, cellClick: handleCellClick },
+    { title: "Estado", field: "estado", width: 150, responsive: 2, cellClick: handleCellClick },
+    { title: "Creado", field: "createdAt", width: 100, responsive: 2, cellClick: handleCellClick },
+    {
       title: "Horario", width: 115, hozAlign: "center",
       formatter: () => {
         return `<img src=${HorarioIcon} alt="horario" style="width: 30px; height: 30px; cursor: pointer;" />`;
@@ -85,50 +103,51 @@ const Teachers = () => {
 
   return (
     <div className='main-container'>
-        <div className='table-container'>
-            <div className='top-table'>
-                <h1 className='title-table'>Docentes</h1>
-                <div className='filter-actions'>
-                    <Search value={filterRut} onChange={handleRutFilterChange} placeholder={'Filtrar por rut'} />
-                    <button className={`add-button ${dataTeacher.length !== 0 ? 'button-disabled' : ''}`} onClick={handleAddTeacherClick} disabled={dataTeacher.length !== 0}>
-                        {dataTeacher.length !== 0 ? (
-                            <FontAwesomeIcon icon={faPlus} style={{ color: "#3E3478" }} />
-                        ) : (
-                            <FontAwesomeIcon icon={faPlus} style={{ color: "#64D7E7" }} />
-                        )}
-                    </button>
-                    <button className={`edit-button ${dataTeacher.length === 0 ? 'button-disabled' : ''}`} onClick={handleClickUpdate} disabled={dataTeacher.length === 0}>
-                        {dataTeacher.length === 0 ? (
-                            <img src={UpdateIconDisable} alt="edit-disabled" />
-                        ) : (
-                            <img src={UpdateIcon} alt="edit" />
-                        )}
-                    </button>
-                    <button className={`delete-button ${dataTeacher.length === 0 ? 'button-disabled' : ''}`} onClick={() => handleDelete(dataTeacher)} disabled={dataTeacher.length === 0}>
-                        {dataTeacher.length === 0 ? (
-                            <img src={DeleteIconDisable} alt="delete-disabled" />
-                        ) : (
-                            <img src={DeleteIcon} alt="delete" />
-                        )}
-                    </button>
-                </div>
-            </div>
-            <Table
-                data={teachers}
-                columns={columns}
-                filter={filterRut}
-                dataToFilter={'rut'}
-                initialSortName={'nombre'}
-                onSelectionChange={handleSelectionChange}
-            />
-            <VentanaHorario 
-              isOpen={isVentanaHorarioOpen} 
-              onClose={() => setIsVentanaHorarioOpen(false)} 
-              horario={horarioSeleccionado} // Pasa el horario seleccionado
-            />
+      <div className='table-container'>
+        <div className='top-table'>
+          <h1 className='title-table'>Docentes</h1>
+          <div className='filter-actions'>
+            <Search value={filterRut} onChange={handleRutFilterChange} placeholder={'Filtrar por rut'} />
+            <button className={`add-button ${dataTeacher.length !== 0 ? 'button-disabled' : ''}`} onClick={handleAddTeacherClick} disabled={dataTeacher.length !== 0}>
+              {dataTeacher.length !== 0 ? (
+                <FontAwesomeIcon icon={faPlus} style={{ color: "#3E3478" }} />
+              ) : (
+                <FontAwesomeIcon icon={faPlus} style={{ color: "#64D7E7" }} />
+              )}
+            </button>
+            <button className={`edit-button ${dataTeacher.length === 0 ? 'button-disabled' : ''}`} onClick={handleClickUpdate} disabled={dataTeacher.length === 0}>
+              {dataTeacher.length === 0 ? (
+                <img src={UpdateIconDisable} alt="edit-disabled" />
+              ) : (
+                <img src={UpdateIcon} alt="edit" />
+              )}
+            </button>
+            <button className={`delete-button ${dataTeacher.length === 0 ? 'button-disabled' : ''}`} onClick={() => handleDelete(dataTeacher)} disabled={dataTeacher.length === 0}>
+              {dataTeacher.length === 0 ? (
+                <img src={DeleteIconDisable} alt="delete-disabled" />
+              ) : (
+                <img src={DeleteIcon} alt="delete" />
+              )}
+            </button>
+          </div>
         </div>
-    <PopupAddTeacher show={isPopupAddOpen} setShow={setIsPopupAddOpen} />
-    <PopupEditTeacher show={isPopupEditOpen} setShow={setIsPopupEditOpen} data={dataTeacher} action={handleUpdate} />
+        <Table
+          data={teachers}
+          columns={columns}
+          filter={filterRut}
+          dataToFilter={'rut'}
+          initialSortName={'nombre'}
+          onSelectionChange={handleSelectionChange}
+        />
+        <VentanaHorario
+          isOpen={isVentanaHorarioOpen}
+          onClose={() => setIsVentanaHorarioOpen(false)}
+          horario={horarioSeleccionado} 
+        />
+      </div>
+      <PopupCopiado message={messageCopied} onClose={() => setMessageCopied('')} />
+      <PopupAddTeacher show={isPopupAddOpen} setShow={setIsPopupAddOpen} />
+      <PopupEditTeacher show={isPopupEditOpen} setShow={setIsPopupEditOpen} data={dataTeacher} action={handleUpdate} />
     </div>
   );
 };
